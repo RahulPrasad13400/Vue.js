0e95c5275f9a9f43cb47a7ec49c7cc822d4d141e
rahulprasad1399 <rahul.prasad@geekywolf.com>
2026-03-08 20:00:14 +0530

    Understanding methods in Vue Apps

--- 2.Basics and Core Concepts/index.js
+++ 2.Basics and Core Concepts/index.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal : 'Finish the course and learn Vue!',
             vueLink : 'https://www.google.com'
         }
+    },
+    methods : {
+        outputGoal(){
+            const randomNumber = Math.random()
+            if(randomNumber < 0.5){
+                return "Breaking Bad"
+            } else {
+                return "Better call saul"
+            }
+        }
     }
 });
 
